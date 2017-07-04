2b9fcf9eb3d1c44e18ea951433b6be973daccfab
Garvit <d.garvit@gmail.com>
2017-07-05 01:07:43 +0530

    Create modules Grid and Game

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -1,8 +1,31 @@
 (function() {
 
-	angular.module('twoZeroFourEight', [])
-	.controller('GameController', function($scope) {
-		
-});
+	angular.module('twoZeroFourEight', ['Game','Grid'])
+	.controller('GameController', function(GameManager) {
+		this.game = GameManager;
+	});
+
+	angular.module('Game', [])
+	.service('GameManager', function() {
+
+	});
+
+	angular.module('Grid', [])
+	.service('GridService', function() {
+		this.grid = [];
+		this.tiles = [];
+
+		this.size = 4;
+	})
+	.directive('grid', function() {
+		return {
+			restrict: 'A',
+			require: 'ngModel',
+			scope: {
+				ngModel: '='
+			},
+			templateUrl: '/static/html/grid.html'
+		};
+	});
 
 })();
