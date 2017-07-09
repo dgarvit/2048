60a41277a28a1929b01aca6d3d3699eba8d3527a
Garvit <d.garvit@gmail.com>
2017-07-10 00:56:50 +0530

    Randomly insert new tiles

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -11,7 +11,7 @@
 		this.grid = a.grid;
 		this.tiles = a.tiles;
 		this.start = function() {
-			a.buildEmptyBoard();
+			a.build();
 		};
 	}]);
 
@@ -25,15 +25,76 @@
 
 		return Tile;
 	})
-	.service('GridService', ['TileModel' , function(a) {
+	.service('GridService', ['TileModel' , function(TileModel) {
 		this.grid = [null];
 		this.tiles = [];
 		this.size = 4;
 		var a = this;
-		this.buildEmptyBoard = function() {
-			for (var x = 0; x < a.size * a.size; x++) {
-				this.grid[x] = null;
+
+		this.withinGrid = function(pos) {
+			return pos.x >=0 && pos.y >=0 && pos.x < a.size && pos.y < a.size;
+		};
+
+		this.get = function(pos) {
+			if(this.withinGrid(pos)) {
+				var i = (pos.y * a.size) + pos.x;
+				return this.tiles[i];
 			}
+			else {
+				return null;
+			}
+		};
+
+		this.set = function(pos, tile) {
+			if (this.withinGrid(pos)) {
+				var i = (pos.y * a.size) + pos.x;
+				this.tiles[i] = tile;
+			}
+		};
+
+		this.availableCells = function() {
+			var cells = [];
+			for (var i = 0; i < a.size; i++) {
+				for (var j = 0; j < a.size; j++) {
+					var b = a.get({x:j, y:i});
+					if(!b) {
+						cells.push({x:j, y:i});
+					}
+				}
+			}
+
+			return cells;
+		};
+
+		this.insertNew = function() {
+			var cells = a.availableCells();
+			if (cells.length > 0) {
+				var i = Math.floor(Math.random() * cells.length);
+				var tile = new TileModel(cells[i], 2);
+				var x = (cells[i].y * a.size) + cells[i].x;
+				this.tiles[x] = tile;
+			}
+		};
+
+		this.remove = function(pos) {
+			var i = (pos.y * a.size) + pos.x;
+			delete this.tiles[i];
+		}
+
+		this.build = function() {
+			for (var i = 0; i < a.size * a.size; i++) {
+				this.grid[i] = null;
+			}
+
+			for (var i = 0; i < a.size; i++) {
+				for (var j = 0; j < a.size; j++) {
+					var x = (i * a.size) + j;
+					a.set({x:j, y:i}, null);
+				}
+			}
+
+			this.insertNew();
+			this.insertNew();
 		};
 	}])
 	.directive('grid', function() {
